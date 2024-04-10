3ffba27e3591e8acebb40094889fda56d65416c1
Michael Rios <mrios93@wgu.edu>
2024-04-10 18:56:36 -0400

    Search and Unit Tet

---
+++ app/src/test/java/com/example/vacationschedulemichaelr/UI/VacationAdapterTest.java
@@ -0,0 +1,21 @@
+package com.example.vacationschedulemichaelr.UI;
+
+import junit.framework.TestCase;
+
+public class VacationAdapterTest extends TestCase {
+
+    public void testOnCreateViewHolder() {
+    }
+
+    public void testOnBindViewHolder() {
+    }
+
+    public void testGetItemCount() {
+    }
+
+    public void testSetVacation() {
+    }
+
+    public void testGetFilter() {
+    }
+}
